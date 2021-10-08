ec91828ec9fc76a7ea1fafa3a8479ddb50a8f6e0
Alex Santos <alexdsantosv@gmail.com>
2021-10-08 04:50:53 -0600

    [ Update ] chemsify api

--- api/cheemsify.js
+++ api/cheemsify.js
@@ -1,22 +1,110 @@
-const cheemsify = (text) => {
-  // REGEX
-  const expr = /[aáeéiíoóuú]+[^aáeéiíoóuúmnñry]/gi;
+function cheemsify(text) {
+  const holyWords = {
+    burger: "burmger",
+    bad: "bamd",
+    batman: "bamtman",
+    cheese: "cheems",
+    cheems: "cheems",
+    cheeseburger: "cheemsburger",
+    doge: "domge",
+    female: "f*male",
+    history: "himstory",
+    nigger: "n-word",
+    nigga: "n-word",
+    retard: "remtard",
+    woman: "w*man",
+    women: "w*men",
+    walter: "walmter",
+    motherfucker: "momtherfumcker",
+  };
 
-  // LISTA DE PALABRAS
-  const wordList = text.split(" ").map((word) => word + " ");
-  const cheemsifiedWordList = wordList.map((word) =>
-    word
-      .replace(expr, (value) =>
-        value.insert(value.length - 1, word.majority() === 1 ? "M" : "m")
-      )
-      .slice(0, -1)
-  );
+  text = text.replace(/(\r\n|\n|\r)/gm, " ");
+  let words = text.split(" ");
+  let cheemedText = [];
+  let symbols = [",", ".", ":", "!", "?", "&", "%", "/"];
 
-  // UNIR PALABRAS
-  return cheemsifiedWordList.join(" ");
-};
+  for (let i = words.length - 1; i >= 0; i--) {
+    let word = words[i].trim().toLowerCase();
+    let needLastCharater = false;
+    let lastChar = word.charAt(word.length - 1);
 
-exports.handler = async (event, _ctx, callback) => {
+    if (symbols.includes(lastChar)) {
+      word = word.slice(0, -1);
+      needLastCharater = true;
+    }
+
+    if (lastChar === "s") {
+      let withoutS = word.slice(0, -1);
+
+      if (holyWords[withoutS]) {
+        word = holyWords[withoutS] + "s";
+        cheemedText[i] = word;
+        continue;
+      }
+    }
+
+    if (holyWords[word]) {
+      word = holyWords[word];
+    } else {
+      word = cheemsAlgorithm(word);
+    }
+
+    if (needLastCharater) {
+      word = word + lastChar;
+    }
+
+    cheemedText[i] = word;
+  }
+
+  return cheemedText.join(" ");
+}
+
+function cheemsAlgorithm(word) {
+  if (word.length < 4) return word;
+
+  let vowels = ["a", "e", "i", "o", "u"];
+  let vowelCount = word.match(/[aeiou]/gi);
+  vowelCount = vowelCount === null ? 0 : vowelCount.length;
+
+  let newWord = [];
+  let addedM = false;
+  let lastChar = word.charAt(word.length - 1);
+
+  for (let i = 0; i < word.length; i++) {
+    let char = word.charAt(i);
+    if (i > 0 && addedM === false) {
+      if (
+        vowelCount > 1 &&
+        i === 1 &&
+        vowels.includes(char) &&
+        !vowels.includes(lastChar)
+      ) {
+        newWord[i] = char;
+        continue;
+      }
+
+      let prev = word.charAt(i - 1);
+      let next = word.charAt(i + 1);
+
+      if (
+        vowels.includes(char) &&
+        next !== "m" &&
+        prev !== "m" &&
+        !vowels.includes(next)
+      ) {
+        char = char + "m";
+
+        addedM = true;
+      }
+    }
+
+    if (newWord[i] === undefined) newWord[i] = char;
+  }
+
+  return newWord.join("");
+}
+
+exports.handler = async (event, _ctx) => {
   // HEADERS
   const headers = {
     "Access-Control-Allow-Origin": "*",
@@ -30,22 +118,22 @@ exports.handler = async (event, _ctx, callback) => {
     const { text } = JSON.parse(event.body);
     if (text.length > 0) {
       const cheemsifiedText = cheemsify(text);
-      callback(null, {
+      return {
         statusCode: 200,
         headers,
         body: JSON.stringify({ message: "Success", cheemsifiedText }),
-      });
+      };
     }
-    callback(null, {
+    return {
       statusCode: 500,
       headers,
       body: JSON.stringify({ message: "Empty string" }),
-    });
+    };
   } else {
-    callback(null, {
+    return {
       statusCode: 405,
       headers,
       body: JSON.stringify({ message: "Method not allowed" }),
-    });
+    };
   }
 };
